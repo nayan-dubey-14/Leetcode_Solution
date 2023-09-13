0fb5b289b56c33b6764bc1cd0d98042a7ae3c7d5
Nayan Dubey <nayandubey08@gmail.com>
2023-09-13 22:53:19 +0530

    BinaryTree: Adding Bottom View Solution

---
+++ bottomViewOfBinaryTree/bottomViewOfBinaryTree.cpp
@@ -0,0 +1,64 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+#include<bits/stdc++.h>
+
+vector<int> bottomView(TreeNode<int> * root){
+    int ic=0;
+    map<int, map<int,vector<int>>> m;
+    queue<pair<TreeNode<int> *, pair<int, int>>> q;
+    int j,i;
+    vector<int> result;
+    TreeNode<int> *t=root;
+    q.push({t,{0,0}});
+    ic++;
+    while(q.size()!=0)
+    {
+        j=1;
+        i=0;
+        while(j<=ic)
+        {
+            auto p=q.front();
+            q.pop();
+            t=p.first;
+            int c=p.second.first;
+            int r=p.second.second;
+            m[c][r].push_back(t->data);
+            if(t->left!=NULL)
+            {
+                q.push({t->left,{c-1,r+1}});
+                i++;
+            }
+            if(t->right!=NULL)
+            {
+                q.push({t->right,{c+1,r+1}});
+                i++;
+            }
+            j++;
+
+        }
+        ic=i;
+    }
+    for(auto k:m)
+    {
+        auto v=k.second.rbegin();
+        result.push_back(*(v->second.rbegin()));
+    }
+    return result;
+}
